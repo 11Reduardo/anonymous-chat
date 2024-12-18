6d4e4682e1b12e44d128a45d006c46ab347d6967
R3duardo <multirandom12@gmail.com>
2024-12-18 12:11:20 -0700

    Error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,10 @@ import { RouterOutlet, RouterLink } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public authService: AuthService) {}
+
+  logout(): void {
+    this.authService.logout(); // Llamar al método logout
+  }
+}
